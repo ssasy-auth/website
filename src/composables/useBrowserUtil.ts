397203cd05b2b45215221750d4ac9ef926e254e0
Oliver Manzi <olivermanzi.dev@gmail.com>
2023-04-22 15:37:09 +0200

    patch: adds links to additional browser stores (v0.6.4)

--- src/composables/useBrowserUtil.ts
+++ src/composables/useBrowserUtil.ts
@@ -1,15 +1,16 @@
-import { computed } from 'vue';
 import FirefoxLogo from '@/assets/images/browsers/logo-firefox.svg';
 import ChromeLogo from '@/assets/images/browsers/logo-chrome.svg';
-import SafariLogo from '@/assets/images/browsers/logo-safari.svg';
 import EdgeLogo from '@/assets/images/browsers/logo-edge.svg';
+import SafariLogo from '@/assets/images/browsers/logo-safari.svg';
 import BraveLogo from '@/assets/images/browsers/logo-brave.svg';
+
+type BrowserName = 'Firefox' | 'Chrome' | 'Edge' | 'Brave' | 'Safari';
 
 export interface Browser {
   /**
    * Browser name
    */
-  name: string;
+  name: BrowserName;
   /**
    * Browser logo
    */
@@ -17,112 +18,109 @@ export interface Browser {
   /**
    * Browser download link. If undefined, browser is not supported
    */
-  download: string | undefined;
+  download?: string;
 }
 
-const FirefoxBrowser: Browser = {
-  name: 'Firefox',
-  logo: FirefoxLogo,
-  download: 'https://addons.mozilla.org/en-US/firefox/addon/ssasy/'
-}
-
-const ChromeBrowser: Browser = {
-  name: 'Chrome',
-  logo: ChromeLogo,
-  download: undefined
-}
-
-const SafariBrowser: Browser = {
-  name: 'Safari',
-  logo: SafariLogo,
-  download: undefined
-}
-
-const EdgeBrowser: Browser = {
-  name: 'Edge',
-  logo: EdgeLogo,
-  download: undefined
-}
-
-const BraveBrowser: Browser = {
-  name: 'Brave',
-  logo: BraveLogo,
-  download: undefined
-}
+const browsers: Browser[] = [
+  {
+    name: 'Firefox',
+    logo: FirefoxLogo,
+    download: 'https://addons.mozilla.org/en-US/firefox/addon/ssasy/'
+  },
+  {
+    name: 'Edge',
+    logo: EdgeLogo,
+    download: 'https://microsoftedge.microsoft.com/addons/detail/ssasy/akbbgfjfbmflebpjficmbmndoejecjln'
+  },
+  {
+    name: 'Brave',
+    logo: BraveLogo,
+    download: 'https://chrome.google.com/webstore/detail/ssasy/iefjfkfbgpoadjkckelcpjkobaamomba'
+  },
+  {
+    name: 'Chrome',
+    logo: ChromeLogo,
+    download: 'https://chrome.google.com/webstore/detail/ssasy/iefjfkfbgpoadjkckelcpjkobaamomba'
+  },
+  {
+    name: 'Safari',
+    logo: SafariLogo,
+    download: undefined
+  }
+]
 
 export function useBrowserUtil() {
-  const isFirefox = computed<boolean>(() => {
-    return navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
-  })
-  
-  const isChrome = computed<boolean>(() => {
-    return navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
-  })
+  /**
+   * 
+   * @returns Returns the name of the current browser
+   */
+  function getCurrentBrowserAgent(): BrowserName | undefined {
+    const agent = navigator.userAgent.toLowerCase();
 
-  const isSafari = computed<boolean>(() => {
-    return navigator.userAgent.toLowerCase().indexOf('safari') > -1;
-  })
-
-  const isEdge = computed<boolean>(() => {
-    return navigator.userAgent.toLowerCase().indexOf('msie') > -1;
-  })
-
-  const isBrave = computed<boolean>(() => {
-    return navigator.userAgent.toLowerCase().indexOf('brave') > -1;
-  })
-
-  function getCurrentBrowser(): Browser | undefined {
-    if (isFirefox.value) {
-      return FirefoxBrowser;
-    } 
-    
-    else if (isChrome.value) {
-      return ChromeBrowser;
-    }
-
-    else if (isEdge.value) {
-      return EdgeBrowser;
-    }
-
-    else if (isBrave.value) {
-      return BraveBrowser;
-    }
-
-    else if (isSafari.value) {
-      return SafariBrowser;
-    }
-    
-    else {
+    switch (true) {
+    case agent.indexOf('firefox') > -1:
+      return 'Firefox';
+    case agent.indexOf('edg/') > -1 || agent.indexOf('msie') > -1 || (agent.indexOf('/') > -1 && agent.indexOf('Edg') > -1):
+      return 'Edge';
+    case agent.indexOf('brave') > -1 || (navigator as any).brave:
+      return 'Brave';
+    case agent.indexOf('chrome') > -1: // ! position is important: must be after 'brave' and 'edge' checks
+      return 'Chrome';
+    case agent.indexOf('safari') > -1:
+      return 'Safari';
+    default:
       return undefined;
     }
   }
 
-  function getAllBrowsers(): Browser[] {
-    return [
-      FirefoxBrowser,
-      ChromeBrowser,
-      EdgeBrowser,
-      BraveBrowser,
-      SafariBrowser
-    ];
+  /**
+   * Returns the current browser object
+   */
+  function getCurrentBrowser(): Browser | undefined {
+    const agent: BrowserName | undefined = getCurrentBrowserAgent();
+
+    switch (agent) {
+    case 'Firefox':
+      return browsers.find(browser => browser.name === 'Firefox');
+    case 'Edge':
+      return browsers.find(browser => browser.name === 'Edge');
+    case 'Brave':
+      return browsers.find(browser => browser.name === 'Chrome');
+    case 'Chrome':
+      return browsers.find(browser => browser.name === 'Chrome');
+    case 'Safari':
+      return browsers.find(browser => browser.name === 'Safari');
+    default:
+      return undefined;
+    }
   }
 
+  /**
+   * Returns all browsers
+   */
+  function getAllBrowsers(): Browser[] {
+    const browsersCopy = browsers.slice();
+
+    const currentBrowser = getCurrentBrowser();
+
+    if(currentBrowser) {
+      // sort browsers so that the current browser is first
+      const currentBrowserIndex = browsersCopy.findIndex(browser => browser.name === currentBrowser?.name);
+      browsersCopy.unshift(browsersCopy.splice(currentBrowserIndex, 1)[0]);
+    }
+
+    return [ ...browsers ];
+  }
+
+  /**
+   * Returns all supported browsers
+   */
   function getSupportedBrowsers(): Browser[] {
-    return [
-      FirefoxBrowser,
-      ChromeBrowser,
-      EdgeBrowser,
-      BraveBrowser,
-      SafariBrowser
-    ].filter(browser => browser.download !== undefined);
+    return getAllBrowsers().filter(browser => browser.download !== undefined);
   }
 
   return {
-    isChrome,
-    isFirefox,
-    isSafari,
-    isEdge,
-    isBrave,
+    getCurrentBrowserAgent,
     getCurrentBrowser,
     getAllBrowsers,
     getSupportedBrowsers
